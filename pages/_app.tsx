a230e42d7d1666a36dab1df0806da596d807e8dd
Arthur Silva <fuscellaarthur@gmail.com>
2021-02-19 21:34:56 -0300

    added google fonts top header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,18 @@ import { ThemeProvider } from 'styled-components';
 import type { AppProps } from 'next/app'
 import Head from 'next/head';
 
-import theme from '../src/themes';
-import GlobalStyle from '../src/themes/GlobalStyle';
+import theme from '@/themes/index';
+import GlobalStyle from '@/themes/GlobalStyle';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Arthur Fuscella Silva</title>
+        <title>Arthur Fuscella Silva - Portfólio</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&family=Raleway&display=swap" rel="stylesheet" />
       </Head>
+
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
