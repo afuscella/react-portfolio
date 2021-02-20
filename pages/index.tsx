2344929329a7b20584c8fa138bb934b7321f3b44
Arthur Silva <fuscellaarthur@gmail.com>
2021-02-20 15:30:59 -0300

    applied box container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`;
+import { Box } from '@/components/foundation/layout/Container';
+import { Cover } from '@/components/commons/Cover';
+import { Grid } from '@/components/foundation/layout/Grid';
 
 export default function Home() {
-  return <Title>My page</Title>;
+  return (
+    <Box
+      flex={1}
+      display="flex"
+      flexWrap="wrap"
+      flexDirection="column"
+      justifyContent="space-between"
+    >
+      <Grid.Container>
+        <Grid.Row>
+          <Cover />
+        </Grid.Row>
+      </Grid.Container>
+    </Box>
+  );
 }
