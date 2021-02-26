8d9a5aaa980b58ccab8bbe7a7103f3d2806bb3b6
Arthur Silva <fuscellaarthur@gmail.com>
2021-02-26 00:47:10 -0300

    updated index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import { Box } from '@/components/foundation/layout/Container';
+
 import { Cover } from '@/components/commons/Cover';
 import { Footer } from '@/components/commons/Footer';
 import { Menu } from '@/components/commons/Menu';
-import { Grid } from '@/components/foundation/layout/Grid';
+import { Projects } from '@/components/commons/Projects';
 
 export default function Home() {
   return (
-    <>
-      <Box
-        flex={1}
-        display="flex"
-        flexWrap="wrap"
-        flexDirection="column"
-        justifyContent="space-between"
-      >
-        <Menu />
-        <Cover />
-        <Grid.Container>
-          <Grid.Row>
-          </Grid.Row>
-        </Grid.Container>
-      </Box>
-      <Footer />
-    </>
+    <Box
+      flex={1}
+      backgroundImage="url(/images/background-light.svg)"
+      backgroundRepeat="repeat"
+      backgroundPosition="center"
+    >
+
+      <Menu />
+      <Cover
+        as={motion.section}
+        transition={{ delay: 0, duration: 0.5 }}
+        variants={{
+          show: { opacity: 1, y: '0' },
+          hidden: { opacity: 0, y: '20%' },
+        }}
+        initial="hidden"
+        animate="show"
+      />
+
+      <Projects/>
+    </Box>
   );
 }
